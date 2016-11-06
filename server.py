1a6728fd3456471643ed0249ca93ca581b539c28
mefrum <mefrum@yandex.ru>
2016-11-06 14:50:17 +0300

    Add real mthreading

--- server.py
+++ server.py
@@ -1,11 +1,21 @@
 import socket
+import threading
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('0.0.0.0', 2222))
 s.listen(10)
-while True:
-    conn, addr = s.accept()
+
+
+def server(conn, addr):
     while True:
         data = conn.recv(1024)
         if (not data) or (data == b'close'): break
         conn.send(data)
     conn.close()
+
+
+while True:
+    conn, addr = s.accept()
+    print('Connection: ', addr)
+    t = threading.Thread(target=server, args=(conn, addr))
+    t.start()
